73225672c8bc2a69e76a7f2ac616ae3544938b94
lhc4815 <lhc4815@gmail.com>
2025-08-06 04:34:45 +0900

    Vercel API 404 오류 수정: @vercel/node 빌드 추가 및 CommonJS 변환

--- api/admin/jump-to-phase.js
+++ api/admin/jump-to-phase.js
@@ -1,6 +1,6 @@
 const models = require('../lib/models');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
