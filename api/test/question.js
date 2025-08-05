20e2b322447c2d64b13b729f3020175b69915eba
lhc4815 <lhc4815@gmail.com>
2025-08-06 05:02:15 +0900

    문항 API 단순화: DB 의존성 제거하고 샘플 문항 데이터 반환

--- api/test/question.js
+++ api/test/question.js
@@ -1,5 +1,3 @@
-const models = require('../../lib/models');
-
 module.exports = async function handler(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).json({ message: 'Method not allowed' });
@@ -17,122 +15,36 @@ module.exports = async function handler(req, res) {
   }
 
   try {
-    // Get surveyType from request or default to 'v1'
-    const surveyType = req.body?.surveyType || req.query?.surveyType || req.headers['x-survey-type'] || 'v1';
-    
-    // Get dynamic models for the survey type
-    const dynamicModels = models.createDynamicDB(surveyType);
-    
-    console.log(`🔍 Fetching question from database: ${surveyType}`);
-
-    let question;
     const upperType = type.toUpperCase();
-    const baseUrl = `https://${req.headers.host}`;
+    console.log(`🔍 Fetching sample question: Type ${upperType}, Question ${questionNumber}`);
 
+    // Return sample questions for testing
     if (upperType === 'A') {
-      question = await dynamicModels.ProblemV1TypeA.findOne({
-        where: { problem_number: questionNumber }
+      return res.json({
+        problem_number: questionNumber,
+        category_main: "언어능력",
+        category_sub: "어휘력",
+        question_text: `이는 Type A 테스트 문항 ${questionNumber}번입니다. 다음 중 가장 적절한 답을 고르세요.`,
+        question_type: 'A'
       });
     } else if (upperType === 'B') {
-      if (questionNumber <= 7) {
-        // For Q1-Q7, return individual questions
-        question = await dynamicModels.ProblemV1TypeB.findOne({
-          where: { problem_number: questionNumber }
-        });
-      } else if (questionNumber === 8) {
-        // For Q8, return Q8-Q9 as a block with common passage and image
-        const questionsB = await dynamicModels.ProblemV1TypeB.findAll({
-          where: { problem_number: [8, 9] },
-          order: [['problem_number', 'ASC']]
-        });
-
-        if (questionsB.length === 0) {
-          return res.status(404).json({ message: 'Type B questions (Q8-Q9) not found.' });
-        }
-
-        const responseData = {
-          question_type: upperType,
-          is_block: true,
-          common_passage: questionsB[0].common_passage,
-          image_url: `${baseUrl}/images/typeB/Table_I.jpg`,
-          questions: questionsB.map(q => {
-            const { correct_answer, common_passage, ...qData } = q.toJSON();
-            return qData;
-          })
-        };
-        return res.json(responseData);
-      } else if (questionNumber === 9) {
-        return res.status(400).json({ message: 'Q9 is accessed via Q8 as a block.' });
-      } else if (questionNumber === 10) {
-        question = await dynamicModels.ProblemV1TypeB.findOne({
-          where: { problem_number: questionNumber }
-        });
-      } else {
-        return res.status(400).json({ message: 'Invalid question number for Type B. Must be 1-10, but Q9 is accessed via Q8.' });
-      }
+      return res.json({
+        problem_number: questionNumber,
+        question_text: `Type B 문항 ${questionNumber}번: 다음 지문을 읽고 물음에 답하세요.`,
+        passage: "이것은 샘플 지문입니다. 실제 시험에서는 여기에 긴 지문이 나타납니다.",
+        option_a: "선택지 A",
+        option_b: "선택지 B", 
+        option_c: "선택지 C",
+        option_d: "선택지 D",
+        question_type: 'B'
+      });
     } else if (upperType === 'C') {
-      if (questionNumber <= 7) {
-        question = await dynamicModels.ProblemV1TypeC.findOne({
-          where: { problem_number: questionNumber }
-        });
-        
-        if (!question) {
-          return res.status(404).json({ message: `Type C Question ${questionNumber} not found.` });
-        }
-
-        const { correct_answer, ...questionData } = question.toJSON();
-        return res.json({
-          ...questionData,
-          question_type: upperType,
-          image_url: `${baseUrl}/images/typeC/Q${questionNumber}.jpg`
-        });
-      } else if (questionNumber === 8) {
-        const questionsC = await dynamicModels.ProblemV1TypeC.findAll({
-          where: { problem_number: [8, 9, 10] },
-          order: [['problem_number', 'ASC']]
-        });
-
-        if (questionsC.length === 0) {
-          return res.status(404).json({ message: 'Type C questions (Q8-Q10) not found.' });
-        }
-
-        const responseData = {
-          question_type: upperType,
-          is_block: true,
-          common_images: [
-            `${baseUrl}/images/typeC/P1.jpg`,
-            `${baseUrl}/images/typeC/P2.jpg`
-          ],
-          questions: questionsC.map(q => {
-            const { correct_answer, ...qData } = q.toJSON();
-            return {
-              ...qData,
-              image_url: `${baseUrl}/images/typeC/Q${q.problem_number}.jpg`
-            };
-          })
-        };
-        return res.json(responseData);
-      } else {
-        return res.status(400).json({ message: 'Invalid question number for Type C. Must be 1-10, but Q8-Q10 are accessed via Q8.' });
-      }
+      return res.json({
+        problem_number: questionNumber,
+        question_type: 'C',
+        image_url: `https://${req.headers.host}/images/typeC/Q${questionNumber}.jpg`
+      });
     }
-
-    if (!question) {
-      return res.status(404).json({ message: 'Question not found.' });
-    }
-
-    // Don't send the correct_answer to the client
-    const { correct_answer, ...questionData } = question.toJSON();
-    
-    // If it's a Type C question, add the image URL
-    if (upperType === 'C') {
-      questionData.image_url = `${baseUrl}/images/typeC/Q${questionNumber}.jpg`;
-    }
-
-    res.json({
-      ...questionData,
-      question_type: upperType
-    });
 
   } catch (error) {
     console.error('Error fetching question:', error);
